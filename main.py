7fe3aec936a0838e4729da5adf6de9226c4ab151
orosmatthew <orosmatthew@pm.me>
2023-02-07 13:55:43 -0500

    Add buzzer

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import board
 import adafruit_dht
 import RPi.GPIO as GPIO
 import pigpio
+from gpiozero import Buzzer;
 
 dht_device = adafruit_dht.DHT11(board.D4)
 servo = 12
@@ -10,11 +11,16 @@ pwm = pigpio.pi()
 pwm.set_mode(servo, pigpio.OUTPUT)
 pwm.set_PWM_frequency(servo, 50)
 
+buzz = Buzzer(17);
+
 while True:
     try:
         temp_c = dht_device.temperature
         temp_f = temp_c * (9 / 5) + 32
         humidity = dht_device.humidity
+        buzz.on();
+        time.sleep(0.02);
+        buzz.off();
         print(f'Temp: {temp_f:.2f} Humidity: {humidity:.2f}')
         if temp_f > 75:
             temp_f = 75
@@ -45,4 +51,4 @@ while True:
     except KeyboardInterrupt:
         pwm.set_PWM_dutycycle( servo, 0 )
         pwm.set_PWM_frequency( servo, 0 )
-    time.sleep(2.0)
+    time.sleep(5.0)
