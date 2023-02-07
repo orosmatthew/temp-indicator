ce1707b6c9728968e360b79e58322fde6ee29673
orosmatthew <orosmatthew@pm.me>
2023-02-07 14:06:31 -0500

    Add config file and parsing

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import board
 import adafruit_dht
 import RPi.GPIO as GPIO
 import pigpio
-from gpiozero import Buzzer;
+from gpiozero import Buzzer
+from configparser import ConfigParser
+
+parser = ConfigParser()
+parser.read("temp.conf")
+temp_interval_seconds = float(parser.get("main", "temp_interval_seconds"))
 
 dht_device = adafruit_dht.DHT11(board.D4)
 servo = 12
@@ -11,16 +16,16 @@ pwm = pigpio.pi()
 pwm.set_mode(servo, pigpio.OUTPUT)
 pwm.set_PWM_frequency(servo, 50)
 
-buzz = Buzzer(17);
+buzz = Buzzer(17)
 
 while True:
     try:
         temp_c = dht_device.temperature
         temp_f = temp_c * (9 / 5) + 32
         humidity = dht_device.humidity
-        buzz.on();
-        time.sleep(0.02);
-        buzz.off();
+        buzz.on()
+        time.sleep(0.02)
+        buzz.off()
         print(f'Temp: {temp_f:.2f} Humidity: {humidity:.2f}')
         if temp_f > 75:
             temp_f = 75
@@ -37,7 +42,7 @@ while True:
         servo_pwm = (2000 - (angle * 2000) / 180) + 500
         if servo_pwm > 2500:
             servo_pwm = 2500
-        if servo_pwm < 600: # should be 500, but 600 seems to work better
+        if servo_pwm < 600:  # should be 500, but 600 seems to work better
             servo_pwm = 600
         print(f'PWM: {servo_pwm:.2f}')
         pwm.set_servo_pulsewidth(servo, servo_pwm)
@@ -49,6 +54,6 @@ while True:
         dht_device.exit()
         raise error
     except KeyboardInterrupt:
-        pwm.set_PWM_dutycycle( servo, 0 )
-        pwm.set_PWM_frequency( servo, 0 )
-    time.sleep(5.0)
+        pwm.set_PWM_dutycycle(servo, 0)
+        pwm.set_PWM_frequency(servo, 0)
+    time.sleep(temp_interval_seconds)
